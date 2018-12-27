dc33e670ba1adfd8e76438f860a40350b57318f1
Parker Grey Addison <pargrey@gmail.com>
2018-12-26 21:51:35 -0800

    Cleaned up js url scraping, deleted old notebook, cleaned up file structure.

--- Scripts/scrape-image-urls.js
+++ Scripts/scrape-image-urls.js
@@ -1,27 +1,21 @@
 // Copy and paste this into the console after conducting a search
 // (and scrolling down to load all desired images)
 
-var script = document.createElement('script');
-script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
-document.getElementsByTagName('head')[0].appendChild(script);
-
 function saveFile (name, type, data) {
     if (data != null && navigator.msSaveBlob)
         return navigator.msSaveBlob(new Blob([data], { type: type }), name);
-    var a = $("<a style='display: none;'/>");
+    var a = document.createElement("a");
     var url = window.URL.createObjectURL(new Blob([data], {type: type}));
-    a.attr("href", url);
-    a.attr("download", name);
-    $("body").append(a);
-    a[0].click();
+    var mouseclick = new MouseEvent("click");
+    a.setAttribute("href", url);
+    a.setAttribute("download", name);
+    a.dispatchEvent(mouseclick);
     window.URL.revokeObjectURL(url);
     a.remove();
 }
 
-setTimeout(function() {
-    images = $(".rg_ic.rg_i");
-    urls = $.map(images, elem => elem.src);
-    text = urls.join("\r\n");
+images = document.getElementsByClassName("rg_ic");
+urls = Array.from(images).map(elem => elem.src);
+text = urls.join("\r\n");
 
-    saveFile("urls.txt", "data:attachment/text", text);
-}, 1000);
+saveFile("urls.txt", "data:attachment/text", text);
